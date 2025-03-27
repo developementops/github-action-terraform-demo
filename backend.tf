5767d6e5d5cbdf3e75c979939baf0df15c35a5d4
pdahiya <pdahiya@qualcomm.com>
2025-03-27 16:18:10 -0700

    updated tfstate file name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "github-actions-demo-pd"
-    key    = "github-actions-demo.tfstate"
+    key    = "github-actions-demo-repo.tfstate"
     region = "us-west-2"
   }
 }
